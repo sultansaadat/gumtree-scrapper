6736d70ab0d7c4bcd2e5c09bbbbadbed07320a53
wasifjahangir <wasif.jahangir@gmail.com>
2015-02-25 01:19:43 +0500

    scrapper to run

--- scrapper.py
+++ scrapper.py
@@ -19,115 +19,153 @@ class SearchListing:
     tel_084 = []
     tel_others = []
 
-    def __init__(self, category, query="", location=""):
-        self.category = category
-        self.query = query    
-        self.location = location
+    def __init__(self):
+        self.category = ""
+        self.query = ""    
+        self.location = ""
 
     def __str__(self):
         return "Search listing"
 
-    def grabURLs(self):
-    	currentPageNumber = 0
-    	totalPages  = 0
-    	
-    	baseURL = "http://www.gumtree.co.za/s-all-the-ads/v1b0p1"
-    	request = requests.get(baseURL, headers=REQUEST_HEADERS)
-    	if request.status_code == 200:
-    		souped = BeautifulSoup(request.text, "html5lib")
-    		pagination = souped.find_all("p", class_="pagination-results-count")
-    		if len(pagination) > 0:
-    			paginationString = pagination[0]
-    			totalPages = int(paginationString.span.string.split(' ')[5].replace(',', ''))
-    			print totalPages
-    		self.parsePage(souped)
-
-    	if totalPages > 0:
-    		# first page is already indexed above and page index on gumTree is not zero based index
-    		for x in range(2,totalPages+2): 
-    			print "parsing Page#: " + str(x)
-    			URL = "http://www.gumtree.co.za/s-all-the-ads/page-%i/v1b0p1" % x 
-    			# print URL
-    			request = requests.get(URL, headers=REQUEST_HEADERS)
-    			if request.status_code == 200:
-    				souped = BeautifulSoup(request.text, "html5lib")
-    				self.parsePage(souped)
-    	return self.urlList
+    def grabURLs(self, from_page, to_page):
+        currentPageNumber = 0
+        totalPages  = 0
+        for x in range(from_page, to_page): 
+            print "parsing Page#: " + str(x)
+            URL = "http://www.gumtree.co.za/s-all-the-ads/page-%i/v1b0p1" % x 
+            # print URL
+            request = requests.get(URL, headers=REQUEST_HEADERS)
+            if request.status_code == 200:
+                souped = BeautifulSoup(request.text, "html5lib")
+                self.parsePage(souped)
+        return self.urlList
 
     def parsePage(self, souped):
-    	divs = souped.find_all("div", class_="view")
-    	if len(divs) > 1:
-    		hyperlinks = divs[1].find_all("a", class_="href-link")
-    		for x in hyperlinks:
-    			self.urlList.append(self.website + x.get("href"))
-    		# ul = divs[0].ul[1]		# first ul is of top ads but we need the second one i.e. paged ul
-    		# for li in ul.li:
-    		# 	title = li.find_all("div", class_="title")
-    		# 	print title.a.get("href")
+        divs = souped.find_all("div", class_="view")
+        if len(divs) > 1:
+            hyperlinks = divs[1].find_all("a", class_="href-link")
+            for x in hyperlinks:
+                self.urlList.append(self.website + x.get("href"))
+            # ul = divs[0].ul[1]        # first ul is of top ads but we need the second one i.e. paged ul
+            # for li in ul.li:
+            #   title = li.find_all("div", class_="title")
+            #   print title.a.get("href")
 
     def grabPhones(self):
-    	for url in self.urlList:
-    		request = requests.get(url, headers=REQUEST_HEADERS)
-    		if request.status_code == 200:
-    			souped = BeautifulSoup(request.text, "html5lib")
-    			telephone = souped.find_all("a", class_="telephone")
-    			if len(telephone) > 0:
-    				href = telephone[0].get("href")
-    				telephone = href[4:]
-    				self.appendTelephone(telephone)
-    					# print "telephones Size: " + str(len(self.telephones)) + " curr: " + href[4:] + " original:" + href
-    	
+        for url in self.urlList:
+            request = requests.get(url, headers=REQUEST_HEADERS)
+            if request.status_code == 200:
+                souped = BeautifulSoup(request.text, "html5lib")
+                telephone = souped.find_all("a", class_="telephone")
+                if len(telephone) > 0:
+                    href = telephone[0].get("href")
+                    telephone = href[4:]
+                    self.appendTelephone(telephone)
+                        # print "telephones Size: " + str(len(self.telephones)) + " curr: " + href[4:] + " original:" + href
+        
     def generateCSVs(self):
-    	self.saveToCSV("telephones_082", self.tel_082)
-    	self.saveToCSV("telephones_083", self.tel_083)
-    	self.saveToCSV("telephones_084", self.tel_084)
-    	self.saveToCSV("telephones_0818", self.tel_0818)
-    	self.saveToCSV("telephones_0810", self.tel_0810)
-    	self.saveToCSV("telephones_others", self.tel_others)
-    	print "Finished"
-	
+        self.saveToCSV("vodacom_082", self.tel_082)
+        self.saveToCSV("mtn_083", self.tel_083)
+        self.saveToCSV("cell_c_084", self.tel_084)
+        self.saveToCSV("vodacom_0818", self.tel_0818)
+        self.saveToCSV("mtn_0810", self.tel_0810)
+        self.saveToCSV("others", self.tel_others)
+        print "Finished"
+    
     def appendTelephone(self, tel):
-    	tel = tel.replace("(", "").replace(")", "").replace(" ", "")
-    	extention = tel[:3]
-    	extention2 = tel[:4]
+        tel = tel.replace("(", "").replace(")", "").replace(" ", "")
+        extention = tel[:3]
+        extention2 = tel[:4]
 
-    	if  tel in self.tel_082 or  tel in self.tel_083 or  tel in self.tel_084 or tel in self.tel_0818 or tel in self.tel_0810:
-    		 print tel + " already exist" 
-    	elif extention == "082" and not tel in self.tel_082:
-    		self.tel_082.append(tel)
-    		# print "082 => " + tel + " ext: " + extention + ", ext2: " + extention2
-    	elif extention == "083" and not tel in self.tel_083:
-    		self.tel_083.append(tel)
-    		# print "083 => " + tel + " ext: " + extention + ", ext2: " + extention2
-    	elif extention == "084" and not tel in self.tel_084:
-    		self.tel_084.append(tel)
-    		# print "084 => " + tel + " ext: " + extention + ", ext2: " + extention2
-    	elif extention2 == "0818" and not tel in self.tel_0818:
-    		self.tel_0818.append(tel)
-    		# print "0818 => " + tel + " ext: " + extention + ", ext2: " + extention2
-    	elif extention2 == "0810" and not tel in self.tel_0810:
-    		self.tel_0810.append(tel)
-    		# print "0810 => " + tel + " ext: " + extention + ", ext2: " + extention2
-    	else:
-    		self.tel_others.append(tel)
-    		# print "others => " + tel + " ext: " + extention + ", ext2: " + extention2
+        if  tel in self.tel_082 or  tel in self.tel_083 or  tel in self.tel_084 or tel in self.tel_0818 or tel in self.tel_others:
+             print ". " ,
+        elif extention == "082" and not tel in self.tel_082:
+            self.tel_082.append(tel)
+            # print "082 => " + tel + " ext: " + extention + ", ext2: " + extention2
+        elif extention == "083" and not tel in self.tel_083:
+            self.tel_083.append(tel)
+            # print "083 => " + tel + " ext: " + extention + ", ext2: " + extention2
+        elif extention == "084" and not tel in self.tel_084:
+            self.tel_084.append(tel)
+            # print "084 => " + tel + " ext: " + extention + ", ext2: " + extention2
+        elif extention2 == "0818" and not tel in self.tel_0818:
+            self.tel_0818.append(tel)
+            # print "0818 => " + tel + " ext: " + extention + ", ext2: " + extention2
+        elif extention2 == "0810" and not tel in self.tel_0810:
+            self.tel_0810.append(tel)
+            # print "0810 => " + tel + " ext: " + extention + ", ext2: " + extention2
+        else:
+            self.tel_others.append(tel)
+            # print "others => " + tel + " ext: " + extention + ", ext2: " + extention2
             
     def saveToCSV(self, filename, tel_list):
-    	
-    	with open(filename + '.csv', 'wb') as csvfile:
-		    fieldnames = ['telephone']
-		    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-		    writer.writeheader()
-		    for tel in tel_list:
-		    	writer.writerow({'telephone': str(tel)})
+        for tel in tel_list:
+            self.append_csv(filename, tel)
+        # with open(filename + '.csv', 'wb') as csvfile:
+           #  fieldnames = ['telephone']
+           #  writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+           #  writer.writeheader()
+           #  for tel in tel_list:
+           #    writer.writerow({'telephone': str(tel)})
 
-	# def saveToCSV(self, filename, tel_list):
-	# 	print "hello"
-		
+    def create_csv(self, filename):
+        with open(filename + '.csv', 'wb') as csvfile:
+            fieldnames = ['telephone']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+    def append_csv(self, filename, telephone):
+        with open(filename + '.csv', 'ab') as csvfile:
+            fieldnames = ['telephone']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'telephone': str(telephone)})
+    def load_csv(self, filename, tel_list):
+        with open(filename + '.csv') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                tel_list.append(row["telephone"])
+        return tel_list
+        
 
 
-test = SearchListing("Audi")
-result = test.grabURLs()
-test.grabPhones()
-test.generateCSVs()
-# print "result Length: " + str(len(result))
+test = SearchListing()
+
+test.create_csv("vodacom_082")
+test.create_csv("vodacom_0818")
+test.create_csv("mtn_083")
+test.create_csv("mtn_0810")
+test.create_csv("cell_c_084")
+test.create_csv("others")
+
+test.tel_082 = test.load_csv("vodacom_082", test.tel_082)
+test.tel_0818 = test.load_csv("vodacom_0818", test.tel_0818)
+test.tel_083 = test.load_csv("mtn_083", test.tel_083)
+test.tel_0810 = test.load_csv("mtn_0810", test.tel_0810)
+test.tel_084 = test.load_csv("cell_c_084", test.tel_084)
+test.tel_others = test.load_csv("others", test.tel_others)
+
+# print test.tel_others
+
+def scrapPages(test, fromIdx, toIdx):
+    test.urlList = []
+    # test.tel_082 = []
+    # test.tel_083 = []
+    # test.tel_084 = []
+    # test.tel_0818 = []
+    # test.tel_0810 = []
+    # test.tel_others = []
+    result = test.grabURLs(fromIdx,toIdx)
+    test.grabPhones()
+    test.generateCSVs()
+    print "Processed "+ str(fromIdx) + " => " + str(toIdx)
+    
+        
+for x in xrange(0,5000):
+    scrapPages(test, x,x+1)
+# scrapPages(test, 2,4)
+# scrapPages(test, 4,6)
+# scrapPages(test, 6,8)
+# scrapPages(test, 8,10)
+# scrapPages(test, 10,12)
+
+
